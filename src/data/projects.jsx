9b200b8d9cdf0bd6aaf6c837542f291b332f4656
Mohit Singh <mscrick01@gmail.com>
2025-04-14 17:53:26 +0530

    Updated Projects

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -3,10 +3,37 @@ export const projects = [
       id: 1,
       title: "Otaku Verse",
       description: "Otaku Haven – A digital paradise for anime enthusiasts to explore, connect, and indulge in their passion!",
-      tech: ["React","Javascript"],
+      tech: ["React Js"],
       image: "https://m.media-amazon.com/images/M/MV5BYmEwNjIyNDItNjVjOS00NjM5LWJlOGUtODI4YWFlZmQ0MjJmXkEyXkFqcGc@._V1_.jpg",
-      link: "https://github.com/MohitSingh250/Otaku-Verse",
+      link: "https://lustrous-granita-3a573f.netlify.app/",
       featured: true
     },
-    // ... other projects
+    {
+      id: 2,
+      title: "Gravix",
+      description: "Gravix is a JEE preparation web app built with React.js and Firebase, offering students chapter-wise practice questions, timed mock tests, and real-time performance tracking to help them prepare effectively for JEE Main and Advanced.",
+      tech: ["React Js","Firebase"],
+      image: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      link: "https://statuesque-custard-b0e73d.netlify.app",
+      featured: true
+    },
+    {
+      id: 3,
+      title: "Allen Clone",
+      description: "Built an Allen AI clone using React.js as my first project to learn core concepts like components, state, and user interaction.",
+      tech: ["React Js"],
+      image: "https://myexam.allen.in/wp-content/uploads/2023/06/ALLEN-Digital-Announces-Scholarship-for-JEE-NEET-Leader-Batches-1280x720.jpg",
+      link: "https://grand-crisp-507212.netlify.app",
+      featured: true
+    },
+    {
+      id: 4,
+      title: "RayBan",
+      description: "Created a modern Rayban landing page using HTML, CSS, JavaScript, with smooth animations powered by GSAP and Shery.js libraries.",
+      tech: ["HTML","CSS",'Javascript'],
+      image: "https://upload.wikimedia.org/wikipedia/commons/d/d1/Ray-Ban_logo.svg",
+      link: "https://preeminent-crisp-3825f1.netlify.app",
+      featured: true
+    }
+
   ];
